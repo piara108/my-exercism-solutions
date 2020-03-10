67818e6161f97d21d2df4d12cee2f19404a923b7
cargoCult108 <piara1085@gmail.com>
2020-03-09 21:02:32 -0700

    Downloaded triangle exercise

---
+++ javascript/triangle/triangle.js
@@ -0,0 +1,21 @@
+// Exercise: Triangle
+
+export class Triangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+
+  isEquilateral() {
+    // return (this.side1 === this.side2 && this.side2 === this.side3);
+  }
+
+  isIsosceles() {
+    
+  }
+
+  isScalene() {
+    
+  }
+}
