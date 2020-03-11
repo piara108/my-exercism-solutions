9dea5149458a8b948ded93dcc256bed0ed74c03d
cargoCult108 <piara1085@gmail.com>
2020-03-10 21:38:51 -0700

    Completed the equilateral and scalene triangle tests

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -8,14 +8,18 @@ export class Triangle {
   }
 
   isEquilateral() {
-    // return (this.side1 === this.side2 && this.side2 === this.side3);
+    return  (this.side1 != 0 || this.side2 != 0 || this.side3 != 0) && 
+            (this.side1 === this.side2 && this.side2 === this.side3);
   }
 
   isIsosceles() {
-    
+    return  (this.side1 === this.side2) ||
+            (this.side2 === this.side3) ||
+            (this.side1 === this.side3);
   }
 
   isScalene() {
-    
+    return  (this.side1 <= (this.side2 + this.side3)) &&
+            (this.side1 != this.side2 && this.side2 != this.side3);
   }
 }
