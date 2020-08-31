c77b63bdbb932ff0dd6987e5ae647daae2eb0262
cargoCult108 <piara1085@gmail.com>
2020-08-30 21:52:13 -0700

    Downloaded word-count and scrabble-score exercises

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,21 +1,2 @@
-# Word Count
-
-import re
-
 def count_words(sentence):
-    scrub_sentence = re.sub(r'[^\w\s+_+]', ' ', sentence.lower()).split()
-    word_count = {}
-
-    for word in scrub_sentence:
-        if word in word_count:
-            word_count[word] += 1
-        else:
-            word_count[word] = 1
-
-    return word_count
-
-# print(count_words('Hello, where, are! we/ going? Hello'))
-# print(count_words("word"))
-# print(count_words("one fish two fish red fish blue fish"))
-print(count_words("one,two,three"))
-# count_words('Hello, where, are! we/ going?')
+    pass
